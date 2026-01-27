32181a407c9cafaa6395c258db96c0f21bd574ae
Your Name <you@example.com>
2026-01-28 05:10:44 +0700

    fix vercel api routing

--- api/index.js
+++ api/index.js
@@ -1,2 +1,25 @@
-const app = require('../cosplayer-backend/server');
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('../config/db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// DB
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
+});
+
+// ROUTES
+app.use('/api/products', require('../routes/products'));
+app.use('/api/orders', require('../routes/orders'));
+app.use('/api/auth', require('../routes/auth'));
+
+app.get('/', (req, res) => {
+  res.send('API OK');
+});
+
 module.exports = app;
